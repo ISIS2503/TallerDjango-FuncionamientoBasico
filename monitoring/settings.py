746daaaa8a15eba6ddc878cdc799a82830da6c13
mateo <m.devia@uniandes.edu.co>
2019-08-07 19:07:15 -0500

    app created

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'measurements'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'monitoring.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'measurements', 'templates', 'measurements')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
